ecbf5762e10def7bfed8375c99c89d28e9228857
Mel <m_hinojosa@rocketmail.com>
2015-12-02 01:02:38 -0600

    Update script
    Added basic script checks

--- install/install-htop.sh
+++ install/install-htop.sh
@@ -1,4 +1,44 @@
-#!/bash/bin
-echo This script will install htop to your WD My Cloud
-echo .
-echo .
+#!/bin/bash
+echo ; echo This script will install htop to your WD My Cloud ; echo 
+# check for root since root is needed to install
+echo [ Checking for root... ]
+case "$EUID" in
+    0) ;;
+    *) echo [ Installation must be run as ROOT user]; exit 1 ;;
+esac
+echo [ Running as root!.... ]; echo   ##############
+
+# check for firmware version ########
+#
+#   if firmware is not 4.xx.xx,  msg: can't run, then exit 
+#
+#################
+
+# check if htop is already installed ########
+echo [ Checking for htop... ]
+case "$(dpkg-query -s -f='${Status}' htop 2>/dev/null | grep -c "ok installed")" in
+    0) ;;
+	*) echo [ htop is already installed, now exiting! ]; exit 1 ;;
+esac ; echo     ################
+
+# check for man folder owner permission bug #####
+#
+#   if bug exists, fix it ; else continue
+#
+#
+##################
+
+#  check for htop deb file #########
+#
+#   if file not exists, msg: missing file,... else Install htop
+#
+#
+#
+#dpkg -i ../Files/htop*.deb
+##########################
+
+#  if you got this far, htop was installed properly
+#
+#
+echo Success EOF
+
